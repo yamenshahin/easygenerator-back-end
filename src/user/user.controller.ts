16594ff4ef8c5a3dfd198a0de14e61fade6e7737
Yamen Shahin <yamenshahin@gmail.com>
2024-06-16 09:58:07 +0300

    "update user controller signUp to create user"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,14 +8,16 @@ import {
   HttpStatus,
 } from '@nestjs/common'
 import {CreateUserDto} from './dto/user.dto'
+import {UserService} from './user.service'
 @Controller('users') // Defines base path for user endpoints (e.g., /users)
 export class UserController {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
 
   @Post()
   @UsePipes(new ValidationPipe())
-  async signup(@Body() createUserDto: CreateUserDto) {
+  async signUp(@Body() createUserDto: CreateUserDto) {
     try {
+      const newUser = await this.userService.create(createUserDto)
       return {message: 'user created successfully'}
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
