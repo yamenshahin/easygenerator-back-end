dddee0560524cf0be9450783a6084853f9547a15
Yamen Shahin <yamenshahin@gmail.com>
2024-06-16 09:29:52 +0300

    "format all files"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import {Module} from '@nestjs/common'
+import {AppController} from './app.controller'
+import {AppService} from './app.service'
+import {UserModule} from './user/user.module'
+import {AuthModule} from './auth/auth.module'
 
 @Module({
   imports: [UserModule, AuthModule],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,24 @@
 import {
-    Controller,
-    Post,
-    Body,
-    UsePipes,
-    ValidationPipe,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common'
-  import {CreateUserDto} from './dto/user.dto'
-  @Controller('users') // Defines base path for user endpoints (e.g., /users)
-  export class UserController {
-    constructor() {}
-  
-    @Post() 
-    @UsePipes(new ValidationPipe()) 
-    async signup(@Body() createUserDto: CreateUserDto) {
-        try {
-            return {message: 'user created successfully'}
-        } catch (error) {
-            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
-        }
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
+import {CreateUserDto} from './dto/user.dto'
+@Controller('users') // Defines base path for user endpoints (e.g., /users)
+export class UserController {
+  constructor() {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  async signup(@Body() createUserDto: CreateUserDto) {
+    try {
+      return {message: 'user created successfully'}
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+    }
   }
 }
