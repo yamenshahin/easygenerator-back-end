22bdf0be2669fa1bc2dcd7932d2d8670c1f17b66
Yamen Shahin <yamenshahin@gmail.com>
2024-06-16 09:19:13 +0300

    intial user DTO for validation

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,19 @@
+import {IsNotEmpty, IsEmail, MinLength, Matches} from 'class-validator'
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @IsNotEmpty()
+  @MinLength(8, {
+    message: 'password must be at least 8 characters long',
+  }) // Minimum 8 characters
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9\s])/, {
+    message:
+      'password must contain a lowercase, uppercase, number, and special character (exclude white space)',
+  })
+  password: string
+}
