6f7ec6094e159eff360288cf890ea186f5e756d5
Yamen Shahin <yamenshahin@gmail.com>
2024-06-16 09:28:41 +0300

    "add intial sign up"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import {
+    Controller,
+    Post,
+    Body,
+    UsePipes,
+    ValidationPipe,
+    HttpException,
+    HttpStatus,
+  } from '@nestjs/common'
+  import {CreateUserDto} from './dto/user.dto'
+  @Controller('users') // Defines base path for user endpoints (e.g., /users)
+  export class UserController {
+    constructor() {}
+  
+    @Post() 
+    @UsePipes(new ValidationPipe()) 
+    async signup(@Body() createUserDto: CreateUserDto) {
+        try {
+            return {message: 'user created successfully'}
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
+  }
+}
